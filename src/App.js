267033b65f6fcf2c12b31b85f76939052e3bf1e0
RenaAFMatos <renatomts262@gmail.com>
2024-12-20 15:40:52 +0000

    Components added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header'
+import Main from './Main.js'
+import Nav from './Nav.js'
+import Footer from './Footer.js'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
